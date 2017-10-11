1c2b042882a95933649a365a4820eb341cc58d59
Oliver Foster <oliver.foster@kineo.com>
2017-10-11 13:54:45 +0100

    Updated

--- js/dataLoader.js
+++ js/dataLoader.js
@@ -2,16 +2,32 @@ define([
   'adapt-contrib-core/js/adapt'
 ], function(Adapt) {
 
+  // This plugin will load all of the json files from the adapt/data/manifest.js file.
+  // It is a precursor to the setup of the models and collections in Adapt
+  // It allows any kind of new json file to be created in the course folder and loaded into Adapt
+
   var DataLoader = Backbone.Controller.extend({
 
     manifest: null,
     entries: null,
     loaded: 0,
+    isReady: false,
+    isAppReady: false,
 
     initialize: function() {
       this.entries = [];
       _.bindAll(this, "onManifestLoaded", "onEntryLoaded");
+      this.setUpEventListeners();
       this.loadManifest();
+    },
+
+    setUpEventListeners: function() {
+      this.listenToOnce(Adapt, "app:ready", this.onAppReady);
+    },
+
+    onAppReady: function() {
+      this.isAppReady = true;
+      this.checkReadyTrigger();
     },
 
     loadManifest: function() {
@@ -26,30 +42,44 @@ define([
     loadManifestEntries: function() {
       this.entries = this.entries || [];
       var loaded = 0;
-      this.manifest.forEach(function(entry) {
-        $.getJSON(entry, function(data) {
-          this.onEntryLoaded(entry, data);
+      this.manifest.forEach(function(file) {
+        $.getJSON(file, function(data) {
+          this.onEntryLoaded(file, data);
         }.bind(this));
       }.bind(this));
     },
 
-    onEntryLoaded: function(entry, data) {
+    onEntryLoaded: function(file, data) {
       this.loaded++;
       if (data instanceof Array) {
-        for (var i = 0, l = data.length; i < l; i++) data[i]._entry = entry;
+        for (var i = 0, l = data.length; i < l; i++) data[i]._file = file;
         this.entries.push.apply(this.entries, data);
       } else {
-        data._entry = entry;
+        data._file = file;
         this.entries.push(data);
       }
-      if (this.loaded === this.manifest.length) {
+      this.checkReadyTrigger();
+    },
+
+    checkReadyTrigger: function() {
+      if (this.loaded !== this.manifest.length) return;
+      if (!this.isAppReady) return;
+      _.defer(function() {
         Adapt.trigger("dataloader:ready");
-      }
+        Adapt.dataLoader.isReady = true;
+      });
+    },
+
+    findByType: function(type) {
+      return _.where(this.entries, { _type: type });
+    },
+
+    findByFile: function(file) {
+      return _.where(this.entries, { _file: file });
     }
 
   });
 
   Adapt.dataLoader = new DataLoader;
-
 
 });
